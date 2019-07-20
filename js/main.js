ff9cce29eb8846af5e59a3d44fe2c6b2d3ca7307
SomethingIsAlreadyTaken <spicemustflow1919@gmail.com>
2019-07-20 20:58:08 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function (){
+	'use strict';
+
+/*	function genRand(min, max) {
+		return Math.round(Math.random() * (max - min)) + min;
+	}
+
+	if (genRand(0, 1)) {
+		document.write("heads");
+	} else {
+		document.write("tails");
+	}*/
+
+})();
