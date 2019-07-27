e1e6ab7ad029aff8e0a2a08b0ac0f0271acf7176
SomethingIsAlreadyTaken <spicemustflow1919@gmail.com>
2019-07-27 12:28:28 +0300

    added js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 (function (){
 	'use strict';
 
-/*	function genRand(min, max) {
+	function genRand(min, max) {
 		return Math.round(Math.random() * (max - min)) + min;
 	}
 
-	if (genRand(0, 1)) {
-		document.write("heads");
-	} else {
-		document.write("tails");
-	}*/
+	let backSide = document.querySelector('.coin__side--tails'),
+		coin     = document.querySelector('.coin')
+	
+	coin.addEventListener('click', setZIndex);
 
+	// setInterval(setZIndex, 1000);
+
+	function setZIndex() {
+		let result = genRand(0,1);
+		backSide.style.zIndex = --result;
+	}
 })();
